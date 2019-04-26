b86cf7a17f64ea062b5215c1159f56f3e021f1a0
bangood <2419281635@qq.com>
2019-04-26 16:37:34 +0800

    temp

--- src/utils/koa.js
+++ src/utils/koa.js
@@ -1,18 +1,30 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import BodyParser from 'koa-bodyparser';
+import AlipaySdk from 'alipay-sdk';
+import path from 'path';
+import fs from 'fs';
+const alipaySdk = new AlipaySdk({
+    appId: '2019042364291281',
+    privateKey: fs.readFileSync(path.resolve(__dirname,'../../assets/private-key.pem'),'ascii'),
+    alipayPublicKey: fs.readFileSync(path.resolve(__dirname,'../../assets/public-key.pem'),'ascii'),
+});
 const app = new Koa();
 const router = new Router();
 
 app.use(BodyParser({ enableTypes: ['json', 'form'] }));
 
-router.post('/gateway.do',(ctx,next)=>{
-    const sign = '';
-    const sign_type = '';
-    const biz_content = '';
-    const service = '';
-    const charset = '';
-    console.log(ctx.request.body);
+router.post('/gateway.do',async (ctx,next)=>{
+    const {sign,charset,biz_content,sign_type,service} = ctx.request.body;
+    try {
+        let bb = `biz_content=${biz_content}&charset=${charset}&sign_type=${sign_type},${service}`;
+        const result = await alipaySdk.checkNotifySign(ctx.request.body)
+        console.log(result);
+    }catch(err){
+        console.log('errrrrrrr')
+        console.log(err);
+    }
+
 });
 app.use(router.routes());
 router.get('/',(ctx,next)=>{
