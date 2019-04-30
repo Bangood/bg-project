dde889b9ba66cc545972bcb256632d7fe2e3c62d
bangood <2419281635@qq.com>
2019-04-30 10:03:22 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import AlipaySdk from 'alipay-sdk';
 import path from 'path';
 import fs from 'fs';
 import {init as koaInit} from './utils/koa';
-const alipaySdk = new AlipaySdk({
-    appId: '2019042364291281',
-    privateKey: fs.readFileSync(path.resolve(__dirname,'../assets/private-key.pem'),'ascii'),
-    alipayPublicKey: fs.readFileSync(path.resolve(__dirname,'../assets/public-key.pem'),'ascii'),
-});
+// const alipaySdk = new AlipaySdk({
+//     appId: '2019042364291281',
+//     privateKey: fs.readFileSync(path.resolve(__dirname,'../assets/private-key.pem'),'ascii'),
+//     alipayPublicKey: fs.readFileSync(path.resolve(__dirname,'../assets/public-key.pem'),'ascii'),
+// });
 
 async function check(){
   
