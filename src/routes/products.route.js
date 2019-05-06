3ea73e6765f5f6e50a44755130e7425572faa5d6
bangood <2419281635@qq.com>
2019-05-06 22:42:23 +0800

    temp

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
-import { list, renderPage, renderItem,getAuthUrl,getProduct } from '../controllers/products.controller';
+import { list, renderPage, renderItem, getAuthUrl, getProduct, saveOrder } from '../controllers/products.controller';
 const router = new Router();
 router.get('/', renderPage)
     .post('/list', list)
-    .post('/getauthurl',getAuthUrl)
-    .post('/getproduct',getProduct)
+    .post('/getauthurl', getAuthUrl)
+    .post('/saveorder', saveOrder)
+    .post('/getproduct', getProduct)
     .get('/:productId', renderItem);
 
 
