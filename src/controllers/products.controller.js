1f148714c1324c75a6ff6628db8b6d18a2ba58bd
bangood <2419281635@qq.com>
2019-05-07 10:39:45 +0800

    no message

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -95,7 +95,7 @@ async function saveOrder($ctx) {
             "data": {
                 "key": 0,
                 "msg": {
-                    body: result2
+                    body: result2[0]
                 }
             }
         })
