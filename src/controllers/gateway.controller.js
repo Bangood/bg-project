def0c7b4597a9c6fcc4d80bb9d38a5245e42bf04
bangood <2419281635@qq.com>
2019-05-10 16:41:53 +0800

    no message

--- src/controllers/gateway.controller.js
+++ src/controllers/gateway.controller.js
@@ -7,7 +7,7 @@ async function verify(ctx) {
         console.log(ctx.request.body);
         const result = await alipaySDK.checkNotifySignForGateway(ctx.request.body);
         if (result) {
-            let sign = await alipaySDK.signForGateway
+            let sign = await alipaySDK.signForGateway()
             ctx.response.type = 'text/xml;charset=GBK'
             return ctx.body = `<?xml version="1.0" encoding="GBK"?><alipay><response><biz_content>${merchantPublicKey}</biz_content><success>true</success></response><sign>${sign}</sign><sign_type>RSA2</sign_type></alipay>`;
 
