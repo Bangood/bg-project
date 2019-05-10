89c65d6654b5e1214d6f96150c0041c3e5df93a6
bangood <2419281635@qq.com>
2019-05-10 16:13:05 +0800

    no message

--- src/controllers/gateway.controller.js
+++ src/controllers/gateway.controller.js
@@ -5,7 +5,7 @@ async function verify(ctx){
     try {
         console.log(ctx.request.body);
         const {sign,service,sign_type,biz_content,charset} = ctx.request.body;
-        const result = await alipaySDK.checkNotifySign(ctx.request.body);
+        const result = await alipaySDK.checkNotifySignForGateway(ctx.request.body);
         console.log(result);
     }catch(err){
         console.log(err);
--- src/utils/AlipaySDK.js
+++ src/utils/AlipaySDK.js
@@ -1,14 +1,18 @@
 import alipaySDK from 'alipay-sdk';
+import crypto from 'crypto';
 import {
     appID,
     merchantPrivateKey,
     merchantPublicKey,
     alipayPublicKey
 } from '../config/alipay.config';
+const ALIPAY_ALGORITHM_MAPPING = {
+    RSA: 'RSA-SHA1',
+    RSA2: 'RSA-SHA256',
+};
 export class AlipaySDK {
     constructor() {
         this.instance = null;
-        console.log('hi')
     }
     static getInstance() {
         if (!this.instance) {
@@ -17,8 +21,31 @@ export class AlipaySDK {
                 privateKey: merchantPrivateKey,
                 alipayPublicKey:alipayPublicKey
             });
+            this.instance.checkNotifySignForGateway= this.checkNotifySignForGateway;
             return this.instance;
         }
         return this.instance;
     }
+    //为网关验签
+    checkNotifySignForGateway(postData) {
+        const signStr = postData.sign;
+        const signType = postData.sign_type || 'RSA2';
+        if (!signStr) {
+            return false;
+        }
+        const signArgs = Object.assign({}, postData);
+        // 除去sign、sign_type 皆是待验签的参数。
+        delete signArgs.sign;
+        const decodeSign = Object.keys(signArgs).sort().filter(val => val).map((key) => {
+            let value = signArgs[key];
+            if (Array.prototype.toString.call(value) !== '[object String]') {
+                value = JSON.stringify(value);
+            }
+            return `${key}=${decodeURIComponent(value)}`;
+        }).join('&');
+        const verifier = crypto.createVerify(ALIPAY_ALGORITHM_MAPPING[signType]);
+        verifier.update(decodeSign, 'utf8');
+        return verifier.verify(alipayPublicKeyy, signStr, 'base64');
+    }
+
 }
