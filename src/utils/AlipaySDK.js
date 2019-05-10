9b47feee03c053eb31ccd7690408c26faf156518
bangood <2419281635@qq.com>
2019-05-10 16:27:15 +0800

    no message

--- src/utils/AlipaySDK.js
+++ src/utils/AlipaySDK.js
@@ -15,35 +15,35 @@ export class AlipaySDK {
         this.instance = null;
     }
     static getInstance() {
-            if (!this.instance) {
-                this.instance = new alipaySDK({
-                    appId: appID,
-                    privateKey: merchantPrivateKey,
-                    alipayPublicKey: alipayPublicKey
-                });
-                //为网关验签
-                this.instance.checkNotifySignForGateway = function(postData) {
-                    const signStr = postData.sign;
-                    const signType = postData.sign_type || 'RSA2';
-                    if (!signStr) {
-                        return false;
+        if (!this.instance) {
+            this.instance = new alipaySDK({
+                appId: appID,
+                privateKey: merchantPrivateKey,
+                alipayPublicKey: alipayPublicKey
+            });
+            //为网关验签
+            this.instance.checkNotifySignForGateway = function(postData) {
+                const signStr = postData.sign;
+                const signType = postData.sign_type || 'RSA2';
+                if (!this.config.alipayPublicKey || !signStr) {
+                    return false;
+                }
+                const signArgs = Object.assign({}, postData);
+                // 除去sign、sign_type 皆是待验签的参数。
+                delete signArgs.sign;
+                const decodeSign = Object.keys(signArgs).sort().filter(val => val).map((key) => {
+                    let value = signArgs[key];
+                    if (Array.prototype.toString.call(value) !== '[object String]') {
+                        value = JSON.stringify(value);
                     }
-                    const signArgs = Object.assign({}, postData);
-                    // 除去sign、sign_type 皆是待验签的参数。
-                    delete signArgs.sign;
-                    const decodeSign = Object.keys(signArgs).sort().filter(val => val).map((key) => {
-                        let value = signArgs[key];
-                        if (Array.prototype.toString.call(value) !== '[object String]') {
-                            value = JSON.stringify(value);
-                        }
-                        return `${key}=${decodeURIComponent(value)}`;
-                    }).join('&');
-                    const verifier = crypto.createVerify(ALIPAY_ALGORITHM_MAPPING[signType]);
-                    verifier.update(decodeSign, 'utf8');
-                    return verifier.verify(alipayPublicKey, signStr, 'base64');
-                };
-                return this.instance;
-            }
+                    return `${key}=${decodeURIComponent(value)}`;
+                }).join('&');
+                const verifier = crypto.createVerify(ALIPAY_ALGORITHM_MAPPING[signType]);
+                verifier.update(decodeSign, 'utf8');
+                return verifier.verify(this.config.alipayPublicKey, signStr, 'base64');
+            };
             return this.instance;
         }
+        return this.instance;
+    }
 }
