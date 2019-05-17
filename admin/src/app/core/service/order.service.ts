fe66facd933a55129a4ea6bdff9f8e05a59805be
bangood <2419281635@qq.com>
2019-05-17 16:42:05 +0800

    no message

--- admin/src/app/core/service/order.service.ts
+++ admin/src/app/core/service/order.service.ts
@@ -4,7 +4,7 @@ import { BaseService } from './base.service';
 @Injectable()
 export class OrderService extends BaseService {
     list(): any {
-        return this.get('/order/list')
+        return this.get('/order/private/list')
             .then(($res: any[] = []) => {
                 console.log($res);
                 return $res;
--- admin/src/app/core/service/product.service.ts
+++ admin/src/app/core/service/product.service.ts
@@ -4,27 +4,27 @@ import { BaseService } from './base.service';
 @Injectable()
 export class ProductService extends BaseService {
     list(): any {
-        return this.get('/product/list')
+        return this.get('/product/public/list')
             .then(($res: any[] = []) => {
                 console.log($res);
                 return $res;
             });
     }
     create(data): any {
-        return this.post('/product', data)
+        return this.post('/product/private/', data)
             .then(res => {
                 return res;
             });
     }
     deleteOne(id): any {
         console.log(id);
-        return this.delete(`/product/${id}`)
+        return this.delete(`/product/private/${id}`)
             .then(res => {
                 return res;
             });
     }
     update(id, data) {
-        return this.post(`/product/${id}`, data)
+        return this.post(`/product/private/${id}`, data)
             .then(res => {
                 return res;
             });
