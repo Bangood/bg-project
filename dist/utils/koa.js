8a8b3d9a867e2473953716752748c1a721f9ee9c
bangood <2419281635@qq.com>
2019-05-17 17:07:03 +0800

    no message

--- dist/utils/koa.js
+++ dist/utils/koa.js
@@ -9,15 +9,11 @@ exports.init = init;
 
 var _koa = _interopRequireDefault(require("koa"));
 
-var _koaRouter = _interopRequireDefault(require("koa-router"));
-
 var _koaBodyparser = _interopRequireDefault(require("koa-bodyparser"));
 
 var _koaCompose = _interopRequireDefault(require("koa-compose"));
 
 var _koaRespond = _interopRequireDefault(require("koa-respond"));
-
-var _alipaySdk = _interopRequireDefault(require("alipay-sdk"));
 
 var _koaViews = _interopRequireDefault(require("koa-views"));
 
@@ -31,20 +27,14 @@ var _cors = _interopRequireDefault(require("@koa/cors"));
 
 var _koaJwt = _interopRequireDefault(require("koa-jwt"));
 
-var _fs = _interopRequireDefault(require("fs"));
+var _koaHelmet = _interopRequireDefault(require("koa-helmet"));
 
-var crypto = require("crypto");
+var _koaConditionalGet = _interopRequireDefault(require("koa-conditional-get"));
 
-var privateKey = 'MIIEpAIBAAKCAQEAy0Ohf6pq+u9SYY/kTt0VffzdtglGFo0mK5cd+l6BzUrX2SFZaSxqaC98hrGYSvx0cjVCztKK+W7Ob7vjYhHk1+zHA8WO2KFSYQrfRPJNzJivLKSu3N7SwGMDW51kGFkVxJqafnBVm/r7wksaCeQkOA8rNFnPF0epv4jPEX3ua4++syFikneYvx0j6zPT7xefLfm858fOwHq+u1ES+xrO/HCxmG3yzwtHFQsqnxlmAHadC4VOBcU45W6rnhVH144+7hVEGieV7u9grRfuhfLZlkYyphMVHoyWsUSbzKN4V3Pha9S0PFQG4p9txKbY9mxbuzkp2WOsopyQ7EBwKf6n2QIDAQABAoIBAD76u4Vk9jS+BlZ4URMmaUxaFxAaG39NSLBk6EL1GV7Ue8CpFp0ByA5z+C97w2YohqQEYKjYj3rtnET+gFuYk8FbeeZvRe1VPsMWrUsdkIF+JXi9pfiFieNWSmoOgD8TCy2ZgZj/kWb9AxiWUIEUESzVdluIICRkbfIMn6wZys/Eclzhry6crf8a4xFrt4OjO0fvXOLN/OhGwI0MmtVpg3Xgu14M4hAVOaIU4po/9tB+1aOuVgI4k/cAPk++8vV5VImn9DBxOuAi1lMmlhYfJ5jCefxrAtUmPIuB+caPKmEnMwcBGkC0MRVH4eUsG1Ws9d7lHEo3fyK/YkFIHNivCIkCgYEA9fybPanXFEVIA9986X3hofsU148+htA55H9hpVLh3SDS87J5YZBvNmbN+PMKqL4nbXkD52hGsqMVKg/vJEz41KrAxlazRUextYRkFh6lsudgWd97KgXyJGEm2RTAVVRdLviKm/u6vJXVSxk/moKlSBxuK0W+O04TZGrTnZeDnvsCgYEA04nRjyHqXL2ytKEZUKs4K84hRgpPi0hHzRmaqNwiV4ELVRKoMSqMT1JNK9VTfCzhtDfCggQb1Wl0StC5Tixavc99XY9YCIX8qRdxyy6uBxdCMnOTXTA9owexxvKS+AsC4FzlxtaKy3Qv74WU+eXEdcf0lT198K8mMgTjLS6bzDsCgYBTI0s8rhh5S2kplfFb4SXPX1MN/+X8HohSiBLyk26vFBb4Mov/w5cy5dAbodkqawpcX5hIL+AlJCVVEAmdVT6MciGxZ0Az3CVd2h8Rss4CB11zQqsX6uaKESPWHsNak6bi5zHBwnS8pE9wkFoE2L8P/jvl+C5xUNQRDmHzU3Ay3QKBgQCBxBqsrkaBq9ETo3vPE6m6nZGl0K9pJub894b7rUbX3Q6hmndY6dv20OsMR6oWE8ZFcqs8bN7SNQWk25PBU846gC1MlqGPYPpUlFA3LySMrwTpxCKTWQOGnjK9c5Ma3E5gajx+lJ/CHT1GtvBjuEKjag0s+/LlEdzhdEQUNHJYfwKBgQCVH3OTQDtfzhFb2g8yw3hDhZopIaPI8dbNKkp7fQxJlAZAVuVS4cb4Lj0lV7i6orRtCL+HsrNH8kCEhXHOTx05wPNbRBKKhsI7z2XLWbNtzScRRCEk6WERRefQIKbFXe+wlqnUf7an3PxwWeCyxHi4I2w8n80Wh5AxA51XAc4naA==';
-var alipayPublicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiULinfbmmNmsPiiBaNGTKl8abplmt62WBhmJ8l4b4eK0FM7TbLETBAzUgXY1OG9PjQBXyaADNzhdR1exU15NTSpnCWlqcQGZFzZ6Kt39p/dWU5+ZmObG1cX3ShN0qzVcbPxKBYlZvPsrbiBUZdTEOInF822ftcl4cp8OK0kdoHz5xYo/Vc6Ek2re85WeYja4zM/Y9ysyH7+ZdrgtLNs5iA+JJW1jTVwqfctc/lq4N7KmHA6WezIX2E9KZiOSbF8UnYk3ZerggaHoKvkRxHFIqqyHk8JT5Me+39ad1DSFS5J16Grq9S0ZjHcVYXN3z0NXHqRP5QMLe9pDxPk17dU+xQIDAQAB';
-var publicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy0Ohf6pq+u9SYY/kTt0VffzdtglGFo0mK5cd+l6BzUrX2SFZaSxqaC98hrGYSvx0cjVCztKK+W7Ob7vjYhHk1+zHA8WO2KFSYQrfRPJNzJivLKSu3N7SwGMDW51kGFkVxJqafnBVm/r7wksaCeQkOA8rNFnPF0epv4jPEX3ua4++syFikneYvx0j6zPT7xefLfm858fOwHq+u1ES+xrO/HCxmG3yzwtHFQsqnxlmAHadC4VOBcU45W6rnhVH144+7hVEGieV7u9grRfuhfLZlkYyphMVHoyWsUSbzKN4V3Pha9S0PFQG4p9txKbY9mxbuzkp2WOsopyQ7EBwKf6n2QIDAQAB';
-var alipaySdk = new _alipaySdk["default"]({
-  appId: '2019042364291281',
-  privateKey: privateKey,
-  alipayPublicKey: alipayPublicKey
-});
+var _koaEtag = _interopRequireDefault(require("koa-etag"));
+
 var app = new _koa["default"]();
-var allMidlewares = (0, _koaCompose["default"])([(0, _koaRespond["default"])(), (0, _koaBodyparser["default"])({
+var allMidlewares = (0, _koaCompose["default"])([(0, _koaHelmet["default"])(), (0, _koaConditionalGet["default"])(), (0, _koaEtag["default"])(), (0, _koaRespond["default"])(), (0, _koaBodyparser["default"])({
   enableTypes: ['json', 'form']
 }), (0, _cors["default"])(), (0, _koaStatic["default"])(_path["default"].join(__dirname, '../../assets')), (0, _koaViews["default"])(__dirname + '/../views', {
   extension: 'html'
@@ -53,26 +43,7 @@ var allMidlewares = (0, _koaCompose["default"])([(0, _koaRespond["default"])(), 
 }).unless({
   path: [/^\/v1\/auth\/login/, /^\/v1\/auth\/register/, /\/v1\/product\/public/, /^\/v1\/products/, /^\/v1\/order\/public/, /^\/v1\/gateway/]
 }), _routes["default"].routes()]);
-app.use(allMidlewares); // 格式化 key
-
-function formatKey(key, type) {
-  var item = key.split('\n').map(function (val) {
-    return val.trim();
-  });
-  console.log(item); // 删除包含 `RSA PRIVATE KEY / PUBLIC KEY` 等字样的第一行
-
-  if (item[0].includes(type)) {
-    item.shift();
-  } // 删除包含 `RSA PRIVATE KEY / PUBLIC KEY` 等字样的最后一行
-
-
-  if (item[item.length - 1].includes(type)) {
-    item.pop();
-  }
-
-  return "-----BEGIN ".concat(type, "-----\n").concat(item.join(''), "\n-----END ").concat(type, "-----");
-}
-
+app.use(allMidlewares);
 global.userInfoMap = new Map();
 
 function init(port) {
