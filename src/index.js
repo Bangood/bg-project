c2b5679d3b2769c7326ba99ecf108377b422a800
bangood <2419281635@qq.com>
2019-05-29 15:55:07 +0800

    删除无效代码

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import config from 'config'
 import {init as koaInit} from './utils/koa';
-console.log(config);
-console.log(process.env.NODE_ENV)
 async function init(){
   koaInit(config.port);
 }
