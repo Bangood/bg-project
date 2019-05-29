695e3588a8e389771c01af8e12eb5ca994e1482a
bangood <2419281635@qq.com>
2019-05-29 15:05:34 +0800

    增加config

--- dist/index.js
+++ dist/index.js
@@ -6,39 +6,9 @@ var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"))
 
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
-var _alipaySdk = _interopRequireDefault(require("alipay-sdk"));
-
-var _path = _interopRequireDefault(require("path"));
-
-var _fs = _interopRequireDefault(require("fs"));
+var _config = _interopRequireDefault(require("config"));
 
 var _koa = require("./utils/koa");
-
-// const alipaySdk = new AlipaySdk({
-//     appId: '2019042364291281',
-//     privateKey: fs.readFileSync(path.resolve(__dirname,'../assets/private-key.pem'),'ascii'),
-//     alipayPublicKey: fs.readFileSync(path.resolve(__dirname,'../assets/public-key.pem'),'ascii'),
-// });
-function check() {
-  return _check.apply(this, arguments);
-}
-
-function _check() {
-  _check = (0, _asyncToGenerator2["default"])(
-  /*#__PURE__*/
-  _regenerator["default"].mark(function _callee() {
-    return _regenerator["default"].wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-  return _check.apply(this, arguments);
-}
 
 function init() {
   return _init.apply(this, arguments);
@@ -47,19 +17,19 @@ function init() {
 function _init() {
   _init = (0, _asyncToGenerator2["default"])(
   /*#__PURE__*/
-  _regenerator["default"].mark(function _callee2() {
-    return _regenerator["default"].wrap(function _callee2$(_context2) {
+  _regenerator["default"].mark(function _callee() {
+    return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
-        switch (_context2.prev = _context2.next) {
+        switch (_context.prev = _context.next) {
           case 0:
-            (0, _koa.init)(80);
+            (0, _koa.init)(_config["default"].port);
 
           case 1:
           case "end":
-            return _context2.stop();
+            return _context.stop();
         }
       }
-    }, _callee2);
+    }, _callee);
   }));
   return _init.apply(this, arguments);
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import AlipaySdk from 'alipay-sdk';
-import path from 'path';
-import fs from 'fs';
+import config from 'config'
 import {init as koaInit} from './utils/koa';
-// const alipaySdk = new AlipaySdk({
-//     appId: '2019042364291281',
-//     privateKey: fs.readFileSync(path.resolve(__dirname,'../assets/private-key.pem'),'ascii'),
-//     alipayPublicKey: fs.readFileSync(path.resolve(__dirname,'../assets/public-key.pem'),'ascii'),
-// });
-
-async function check(){
-  
-}
 async function init(){
-  koaInit(8000);
+  koaInit(config.port);
 }
 init();
