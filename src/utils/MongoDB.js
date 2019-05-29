52fdfd2c427b5eb8f3ddaf48e5947476bd51e628
bangood <2419281635@qq.com>
2019-05-29 10:17:45 +0800

    解决mongodb topology was destroyed错误

--- src/utils/MongoDB.js
+++ src/utils/MongoDB.js
@@ -6,12 +6,12 @@ export class MongoDB{
     }
     static getInstance(){
         if(!this.instance){
-            this.instance = mongoose.createConnection('mongodb://bangood:alipay15196634454@ds155076.mlab.com:55076/alipay', { useNewUrlParser: true, useFindAndModify: false });
+            this.instance = mongoose.createConnection('mongodb://bangood:alipay15196634454@ds155076.mlab.com:55076/alipay', { useNewUrlParser: true, useFindAndModify: false,reconnectTries: Number.MAX_VALUE,reconnectInterval: 500, poolSize: 10,useCreateIndex: true,autoIndex: false});
             this.instance.once('open',function(){
-                console.log('hi')
+                console.log('数据库已连接')
             })
             this.instance.once('close',function(){
-                console.log('hiclose')
+                console.log('数据库已断开')
             })
         }
         return this.instance
